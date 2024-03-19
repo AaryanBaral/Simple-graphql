8ff85eebfe4a695c4b6349b7957dd84d2ce8bb12
Aaryan Baral <aryanbaral1100@gmail.com>
2024-03-19 16:04:12 +0545

    prisma user model created

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { ApolloServer } from "@apollo/server";
 import {expressMiddleware} from "@apollo/server/express4"
 import bodyParser from "body-parser";
+import { prismaClient } from "./lib/db";
 
 const PORT = Number(process.env.PORT) || 3000
 
@@ -18,11 +19,39 @@ async function init(){
                 hello:String
                 say(name:String):String
             }
+            type Mutation{
+                createUser(firstName:String!,lastName:String!,email:String!,password:String!):Boolean
+            }
         `,
         resolvers:{
             Query:{
                 hello:()=>  ("hey there i am a graph ql server"),
                 say:(_,{name}:{name:string})=> `hey ${name} how are you!`
+            },
+            Mutation:{
+                createUser:async(_,
+                    {
+                        firstName,
+                        lastName,
+                        email,
+                        password}:
+                    {
+                        firstName:string,
+                        lastName:string,
+                        email:string,
+                        password:string}
+                ) => {
+                    await prismaClient.user.create({
+                        data:{
+                            firstName,
+                            lastName,
+                            email,
+                            password,
+                            salt:"random_salt"
+                        }
+                    });
+                    return true;
+                }
             }
         }
     })
